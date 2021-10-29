ce482ebdad270d276d407cf1e2d18aaf02395da3
Karol <ironhide8391@googlemail.com>
2021-10-29 22:21:55 +0100

    Add files via upload

---
+++ TestAppUWP/Views/CustomTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TestAppUWP.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TestAppUWP.Views
+{
+    class CustomTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item != null)
+            {
+                var viewModeltype = item.GetType();
+
+                foreach (var rd in Application.Current.Resources.MergedDictionaries)
+                {
+                    foreach (var value in rd.Values)
+                    {
+                        if (value is DataTemplate dataTempate)
+                        {
+                            Type contexType = dataTempate.GetValue(ViewModelContext.TypeProperty) as Type;
+
+                            if (contexType != null && contexType == viewModeltype)
+                            {
+                                return dataTempate;
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ TestAppUWP/Views/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TestAppUWP.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestAppUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel _mainViewModel;
+
+        public DataContextSwitcher Switcher => _mainViewModel.Switcher;
+  
+        public MainPage()
+        {
+            _mainViewModel = new MainViewModel();
+            Switcher.SwitchVmContext(_mainViewModel.PageOneVM);
+            this.InitializeComponent();
+        }
+
+        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            NavigationViewItem navigationViewItem = (NavigationViewItem)args.InvokedItemContainer;
+
+            if (navigationViewItem == PageOneItem)
+            {
+                Switcher.SwitchVmContext(_mainViewModel.PageOneVM);
+            }
+            else if (navigationViewItem == PageTwoItem)
+            {
+                Switcher.SwitchVmContext(_mainViewModel.PageTwoVM);
+            }
+           
+        }
+    }
+}
